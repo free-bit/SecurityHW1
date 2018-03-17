18abb71b1a96619c7cf50115f1a44811727e4f3b
free-bit <erkin.turkoz@metu.edu.tr>
2018-03-17 22:41:40 +0300

    v2 based on algorithm on the web

--- AESAttack.py
+++ AESAttack.py
@@ -21,32 +21,32 @@ def attack(plain, cipher):
   for val in range(256):
     possible_values.append(val)
 
-  t = [None]*16
+  p = [None]*16
 
   orig_cipher = cipher[:]
-  #orig_cipher_block = cipher[16:32]
+  orig_cipher_block = cipher[16:32]
 
   for i in range(1, 17):
     c_index = 32-i                                          #From 31 to 16 
-    t_index = 16-i                                          #From 15 to 0 
+    p_index = 16-i                                          #From 15 to 0 
     init_val=cipher[c_index]                                #CipherText[31->16]
     for val in possible_values:                             
       cipher=cipher[:c_index]+chr(val)+cipher[c_index+1:]   #Replacing cipher[29]: cipher[:29]+chr(val)+cipher[30:]
       if(decr(cipher)=="SUCCESS" and val!=ord(init_val)):   #Check if padding correct
         print val
-        t[t_index]=val^(i)                                  #t<-decrypted
-        if(t_index>0):                                      #t_index==0 => terminate
+        p[p_index]=orig_cipher_block[p_index]^val^(i)       #p<-decrypted
+        if(p_index>0):                                      #p_index==0 => terminate
           cipher_middle = ''                                #Make replacement in cipher in the required parts
           for j in range(0, i):                             #Set these parts of cipher using t
-            cipher_middle+=chr(t[t_index+j]^(i+1))          
+            cipher_middle+=chr(p[p_index+j]^orig_cipher_block[p_index+j]^(i+1))          
           cipher=cipher[:c_index]+cipher_middle+cipher[32:] #Reform cipher
         break;
-  print t
-  new_middle_block=xor(t, pkcs7(plain), orig_cipher[16:32])
-  new_cipher=orig_cipher[:16]+new_middle_block+orig_cipher[32:]
-  print len(new_cipher)
-  decrytion = AES.new(key, AES.MODE_CBC, iv)
-  print decrytion.decrypt(new_cipher)
+  print p
+  # new_middle_block=xor(t, pkcs7(plain), orig_cipher[16:32])
+  # new_cipher=orig_cipher[:16]+new_middle_block+orig_cipher[32:]
+  # print len(new_cipher)
+  # decrytion = AES.new(key, AES.MODE_CBC, iv)
+  # print decrytion.decrypt(new_cipher)
   # init_val=cipher[31]
   # for val in possible_values:
   #   cipher=cipher[:31]+chr(val)+cipher[32:]
