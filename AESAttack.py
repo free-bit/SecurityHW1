e6d07a0dc9b210384939f022ad0c6ad4b2b7ab61
free-bit <erkin.turkoz@metu.edu.tr>
2018-11-18 12:52:09 +0300

    implemented

--- AESAttack.py
+++ AESAttack.py
@@ -2,96 +2,101 @@
 import sys
 from vulnerable import *
 
-def padding(plain):
+#pad plain text with given pad_len if required
+#return padded string
+def padding(plain, pad_len):
   pads      = ['\x01','\x02','\x03','\x04','\x05','\x06','\x07', \
                '\x08','\x09','\x0A','\x0B','\x0C','\x0D','\x0E','\x0F']
-  plain_len = len(plain)
-  pad_len   = 16 - plain_len
-  return plain+pads[pad_len-1]*pad_len
+  if(pad_len>0):
+    return plain+pads[pad_len-1]*pad_len
+  return plain
 
-def xor(ordDP, ordPlain, ordOCB2):
+#xor two input lists of ints 
+#return a string
+def xor(ordDP, t):
+  len_dp=len(ordDP)
+  len_t=len(t)
   new_cipher_block = ''
-  for i in range(16):
-    new_cipher_block += chr(ordDP[i] ^ ordPlain[i] ^ ordOCB2[i])
+  if(len_dp!=len_t):
+    print "Error!"
+    return None
+  for i in range(len_dp):
+    new_cipher_block += chr(ordDP[i] ^ t[i])
   return new_cipher_block
-
 
 def attack(DP, cipher):
   possible_values = [] 
   for val in range(256):
     possible_values.append(val)
 
-  orig_plain = [None]*16
-  orig_cipher = cipher[:]
-  orig_cipher_block_2 = orig_cipher[16:32]
+  t = [None]*16           #t will be max size of 16 and hold list of int vals
+  orig_cipher = cipher[:] #copy original cipher, it will change during attack
+  pad_len=0               #required pad length for DP
 
   for i in range(1, 17):
-    c_index     = 32-i                                      #From 31 to 16 
-    plain_index = 16-i                                      #From 15 to 0 
-    init_val=cipher[c_index]                                #CipherText[31->16]
+    c_index = 32-i                                          #from 31 to 16 
+    t_index = 16-i                                          #from 15 to 0 
+    init_val=cipher[c_index]                                #cipher[31->16]
     found = False                                           #true if one value other than itself is found
     #try all possible values
     for val in possible_values:                             
-      cipher=cipher[:c_index]+chr(val)+cipher[c_index+1:]                      #Replacing cipher[29]: cipher[:29]+chr(val)+cipher[30:]
-      if(decr(cipher)=="SUCCESS" and val!=ord(init_val)):                      #Check if padding correct
-        orig_plain[plain_index]=ord(orig_cipher_block_2[plain_index])^val^(i)  #orig_plain<-decrypted
-        if(plain_index>0):                                                     #plain_index==0 => terminate
-          cipher_middle = ''                                                   #Make replacement in cipher in the required parts
-          for j in range(0, i):                                                #Set these parts of cipher using orig_plain
-            cipher_middle+=chr(orig_plain[plain_index+j]\
-                           ^ord(orig_cipher_block_2[plain_index+j])\
-                           ^(i+1))          
-          cipher=cipher[:c_index]+cipher_middle+cipher[32:]                    #Reform cipher
+      cipher=cipher[:c_index]+chr(val)+cipher[c_index+1:]   #replacing cipher[29]: cipher[:29]+chr(val)+cipher[30:]
+      if(decr(cipher)=="SUCCESS" and val!=ord(init_val)):   #check if padding correct
+        t[t_index]=val^(i)                                  #t<-decrypted
+        if(t_index>0):                                      #t_index==0 => terminate
+          cipher_middle = ''                                #make replacement in cipher in the required parts
+          for j in range(0, i):                             #set these parts of cipher using t
+            cipher_middle+=chr(t[t_index+j]^(i+1))          
+          cipher=cipher[:c_index]+cipher_middle+cipher[32:] #reform cipher
         found = True
         break;
     #if no value other than itself is found
+    #this means padding ends from this point on 
     if not found:
+      pad_len=i
       val = ord(init_val)
-      orig_plain[plain_index]=ord(orig_cipher_block_2[plain_index])^val^(i)  #orig_plain<-decrypted
-      if(plain_index>0):                                                     #plain_index==0 => terminate
-        cipher_middle = ''                                                   #Make replacement in cipher in the required parts
-        for j in range(0, i):                                                #Set these parts of cipher using orig_plain
-          cipher_middle+=chr(orig_plain[plain_index+j]\
-                         ^ord(orig_cipher_block_2[plain_index+j])\
-                         ^(i+1))          
-        cipher=cipher[:c_index]+cipher_middle+cipher[32:]                    #Reform cipher
-
-  #orig_plain holds list of int vals for each char in original plain text
-  #DP is non-padded string
-  padded_DP=pkcs7(DP)
+      t[t_index]=val^(i)                                    #t<-decrypted
+      if(t_index>0):                                        #t_index==0 => terminate
+        cipher_middle = ''                                  #make replacement in cipher in the required parts
+        for j in range(0, i):                               #set these parts of cipher using t
+          cipher_middle+=chr(t[t_index+j]^(i+1))          
+        cipher=cipher[:c_index]+cipher_middle+cipher[32:]   #reform cipher
+  #DP is non-padded string, pad it wrt pad_len found
+  padded_DP=padding(DP, pad_len)
   #convert into list of int vals
-  ordDP = [ord(each) for each in padded_DP]
-  #orig_cipher_block_2 holds string of this block
-  ordOCB2 = [ord(each) for each in orig_cipher_block_2]
-  new_cipher_block = xor(ordDP, orig_plain, ordOCB2)
+  ordDP=[ord(each) for each in padded_DP]
+  #use only required parts of t (from the end) 
+  new_t_len=len(padded_DP)
+  #drop unnecessary parts from the beginning
+  new_t_start=len(t)-new_t_len
+  #find changed part (from the end) of cipher block
+  new_cipher_block = xor(ordDP, t[new_t_start:])
+  #find length of changed and unchanged parts of block 2 (from the beginning)
+  change_len=len(new_cipher_block)
+  unchanged_len=16+16-change_len
   #combine
-  ciphertext = orig_cipher[:16]+new_cipher_block+orig_cipher[32:]
-
-  #Original plain text
-  orig_plaintext = ''
-  for each in orig_plain:
-    if(each<31):
-      orig_plaintext+='\\x{}'.format(chr(each).encode('hex'))
+  new_ciphertext = orig_cipher[:unchanged_len]+new_cipher_block+orig_cipher[32:]
+  # formatted_ciphertext = ''
+  # for x in ciphertext:
+  #   formatted_ciphertext+='\\x{}'.format(x.encode('hex'))
+  # print formatted_ciphertext
+  x=AES.new(key, AES.MODE_CBC, iv).decrypt(new_ciphertext)
+  formatted = ''
+  for each in x:
+    if(ord(each)<32):
+      formatted+='\\x{}'.format(each.encode('hex'))
     else:
-      orig_plaintext+=chr(each)
-  print orig_plaintext
-
-  formatted_ciphertext = ''
-  for x in ciphertext:
-    formatted_ciphertext+='\\x{}'.format(x.encode('hex'))
-  print formatted_ciphertext
-  print AES.new(key, AES.MODE_CBC, iv).decrypt(ciphertext)
+      formatted+=each
+  print formatted
 
 def main(argv): 
-    # print 'Number of arguments:', len(argv), 'arguments.'
-    # print 'Argument List:', str(argv)
     cipher=argv[0] #Read as byte string (there are unprintable chars)
-    dp=argv[1] 		 #Read as string 
-    print len(cipher)
-    print len(dp)
+    dp=argv[1]     #Read as string 
     attack(dp,cipher)
 
-# NOTE: Commandline args have to be passed with enclosing quotes. 
+# NOTE: command line args have to be passed with enclosing quotes. 
 # Otherwise special characters like space or tab is evaluated by terminal
 if __name__ == "__main__":
+    # print 'Number of arguments:', len(argv), 'arguments.'
+    # print 'Argument List:', str(argv)
     main(sys.argv[1:])
